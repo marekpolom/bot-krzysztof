d5e9358cdc9c24aa768d770555ef9bb2f0f196c8
press2210 <37964992+press2210@users.noreply.github.com>
2018-04-02 01:58:17 +0200

    Add files via upload

---
+++ commands/zapro.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    let invite = "https://discord.gg/gUA3BwE";
+    let zapro = new Discord.RichEmbed()
+    .setColor("#f9f102")
+    .addField("Zaproszenie: ", invite)
+        
+    message.channel.send(zapro);
+}
+ 
+module.exports.help = {
+  name: "zapro"
+}
