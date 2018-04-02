7ea212d02a929d00dd8b0de009daf2384cf9a4a5
press2210 <37964992+press2210@users.noreply.github.com>
2018-04-02 19:58:52 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!args[2]) return message.reply("Zadaj pełne pytanie!");
+  if(!args[1]) return message.reply("Zadaj pełne pytanie!");
   let replies = ["Tak.", "Nie.", "Nie ma chuja.", "Passat 1.9 TDI.", "Nie wiem.", "Nie mam czasu!", "Kosa to pedał!", "XD"];
 
   let result = Math.floor((Math.random() * replies.length));
-  let question = args.slice(1).join(" ");
+  let question = args.slice(0).join(" ");
 
   let ballEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.tag)
